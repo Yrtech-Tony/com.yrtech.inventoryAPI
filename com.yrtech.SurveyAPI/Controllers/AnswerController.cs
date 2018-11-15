3662eb8951865aa32b4df35d52acd9835946491b
Tony <moujunsheng15@163.com>
2018-11-15 19:14:36 +0800

    方法

---
+++ com.yrtech.SurveyAPI/Controllers/AnswerController.cs
@@ -0,0 +1,68 @@
+﻿using System.Web.Http;
+using com.yrtech.InventoryAPI.Service;
+using com.yrtech.InventoryAPI.Common;
+using System.Collections.Generic;
+using System;
+using com.yrtech.InventoryAPI.DTO;
+using System.Threading;
+using Purchase.DAL;
+using System.Web;
+using System.Net.Http;
+using System.Net;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace com.yrtech.SurveyAPI.Controllers
+{
+
+    [RoutePrefix("inventory/api")]
+    public class AnswerController : ApiController
+    {
+        AnswerService answerService = new AnswerService();
+        MasterService masterService = new MasterService();
+        #region APP端
+        /// <summary>
+        /// 清单查询
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        [Route("Answer/GetShopAnswerList")]
+        public APIResult GetShopAnswerList(string projectId, string shopId, string vinCode, string allChk, string addChk)
+        {
+            try
+            {
+                List<AnswerDto> shopAnswerList = answerService.GetShopAnswerList(projectId, shopId, allChk, addChk, vinCode);
+                return new APIResult() { Status = true, Body = CommonHelper.Encode(shopAnswerList) };
+            }
+            catch (Exception ex)
+            {
+                return new APIResult() { Status = false, Body = ex.Message.ToString() };
+            }
+        }
+        /// <summary>
+        /// 获取销售顾问信息
+        /// </summary>
+        /// <param name="projectId"></param>
+        /// <param name="shopId"></param>
+        /// <returns></returns>
+        [Route("Answer/GetShopShopConsultant")]
+        public APIResult GetShopShopConsultant(string projectId, string shopId)
+        {
+            try
+            {
+                List<ShopConsultantDto> shopContantList = answerService.GetShopConsultant(projectId, shopId);
+                foreach (ShopConsultantDto shopContant in shopContantList)
+                {
+                    shopContant.ShopConsultantSubjectLinkList = answerService.GetShopConsultantSubjectLink(projectId, shopContant.ConsultantId.ToString());
+                }
+                return new APIResult() { Status = true, Body = CommonHelper.Encode(shopContantList) };
+            }
+            catch (Exception ex)
+            {
+                return new APIResult() { Status = false, Body = ex.Message.ToString() };
+            }
+        }
+        #endregion
+    }
+}
--- com.yrtech.SurveyAPI/DTO/Answer/AnswerDto.cs
+++ com.yrtech.SurveyAPI/DTO/Answer/AnswerDto.cs
@@ -8,38 +8,27 @@ namespace com.yrtech.InventoryAPI.DTO
     [Serializable]
     public class AnswerDto
     {
-        public int AnswerId { get; set; }
-        public int ProjectId { get; set; }
-        public int SubjectId { get; set; }
-        public string SubjectTypeCode { get; set; }
-        public int ShopId { get; set; }
-        public decimal? PhotoScore { get; set; }
-        public decimal? Score { get; set; }
-        public decimal? ConsultantScore { get; set; }
-        public string InspectionStandardResult { get; set; }
-        public string FileResult { get; set; }
-        public string LossResult { get; set; }
-        public string ShopConsultantResult { get; set; }
+        public string AnswerId { get; set; }
+        public string ProjectId { get; set; }
+        public string ProjectCode { get; set; }
+        public string ProjectName { get; set; }
+        public string ShopId { get; set; }
+        public string ShopCode { get; set; }
+        public string ShopName { get; set; }
+        public string VinCode { get; set; }
+        public string VinCode8 { get; set; }
+        public string ModelName { get; set; }
+        public string SubModelName { get; set; }
+        public string StockAge { get; set; }
+        public string SaleFlag { get; set; }
+        public string VinPhotoName { get; set; }
+        public string SaleInvoicePhotoName { get; set; }
+        public string CarBackPhotoName { get; set; }
         public string Remark { get; set; }
-        public int InUserId { get; set; }
-        public DateTime InDateTime { get; set; }
-        public int ModifyUserId { get; set; }
+        public bool AddChk { get; set; }
+        public string ModifyUserId { get; set; }
         public DateTime ModifyDateTime { get; set; }
-        public DateTime UploadDate { get; set; }
-        public int UploadUserId { get; set; }
-        public string SubjectCode { get; set; }
-        public int SubjectTypeExamId { get; set; }
-        public string SubjectTypeExamName { get; set; }
-        public int SubjectRecheckTypeId { get; set; }
-        public int SubjectLinkId { get; set; }
-        public string SubjectLinkName { get; set; }
-        public int OrderNO { get; set; }
-        public string Implementation { get; set; }
-        public string CheckPoint { get; set; }
-        public string Desc { get; set; }
-        public string AdditionalDesc { get; set; }
-        public string InspectionDesc { get; set; }
-        public char ModifyType { get; set; }//"U"：修改；"D":删除; 
-
+        public string InUserID { get; set; }
+        public DateTime InDateTime { get; set; }
     }
 }
